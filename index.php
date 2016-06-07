083c390cdb488a9ee60806e15f4ffd6dc000c6da
Kejie Shao <kjshao@dicp.ac.cn>
2016-06-07 10:44:40 +0800

    new

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Kejie Shao's Home Page</title>
+  <meta name="author" content="Kejie Shao">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="./bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="./jquery/1.12.4/jquery.min.js"></script>
+  <script src="./bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body id="PageHead">
+<!-- jumbotron -->
+<div class="jumbotron text-center">
+  <br><br>
+  <div class="container-fluid">
+    <h2>Kejie Shao's Home Page</h2> 
+  </div>
+</div>
+<div id="links" class="container-fluid">
+<ul class="list-group">
+  <li class="list-group-item">
+    <a href="http://kjshao.xyz/Calendar/field/index.php">化物所礼堂信息查询系统</a>
+  </li>
+</ul>
+</div>
+</body>
+</html>
