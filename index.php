8533af93ccc0c8f73360e4eb86083471f9e7a08c
Kejie Shao <kjshao@dicp.ac.cn>
2016-07-14 00:20:22 +0800

    new
    new

--- index.php
+++ index.php
@@ -8,14 +8,20 @@
   <link rel="stylesheet" href="./bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="./jquery/1.12.4/jquery.min.js"></script>
   <script src="./bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="Kejie/zoom.css" rel="stylesheet">
+  <link href="Kejie/custom.css" rel="stylesheet">
+  <script src="Kejie/zoom.js"></script>
 </head>
 <body id="PageHead">
 <!-- jumbotron -->
 <div class="jumbotron text-center">
   <br><br>
   <div class="container-fluid">
-    <h2>Welcome!</h2>
+    <h1>Welcome!</h1>
   </div>
+</div>
+<div class="container-fluid">
+ <h2></h2>
 </div>
 <div id="links" class="container-fluid">
 <ul class="list-group">
@@ -23,9 +29,28 @@
     <a href="http://kjshao.xyz/Calendar">化物所礼堂信息查询系统</a>
   </li>
   <li class="list-group-item">
-    <a href="https://github.com/kjshao">My GitHub Home Page</a>
+    <a href="https://github.com/kjshao">My GitHub</a>
   </li>
 </ul>
+</div>
+<div class="container-fluid">
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="Kejie/1.png" data-action="zoom">
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="Kejie/2.png" data-action="zoom">
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="Kejie/3.png" data-action="zoom">
+    </div>
+  </div>
+</div>
 </div>
 </body>
 </html>
